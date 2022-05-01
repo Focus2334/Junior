4691e635467358ffc46ce932c65b81feb85c75c8
Focus2334 <51910536+Focus2334@users.noreply.github.com>
2022-05-01 21:52:54 +0500

    Add files via upload

---
+++ Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool trueValue = false;
+        bool falseValue = true;
+        Console.WriteLine("true = " + trueValue);
+        Console.WriteLine("false = " + falseValue);
+        bool container = trueValue;
+        trueValue = falseValue;
+        falseValue = container;
+        Console.WriteLine("после смены");
+        Console.WriteLine("true = " + trueValue);
+        Console.WriteLine("false = " + falseValue);
+
+    }
+}
