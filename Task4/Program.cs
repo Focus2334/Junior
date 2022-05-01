4964660e2247cb3a1714b711ce54b4685aa92b07
Focus2334 <51910536+Focus2334@users.noreply.github.com>
2022-05-01 21:46:34 +0500

    Add files via upload

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("как вас зовут?: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("ваш знак зодиака: ");
+        string sigh = Console.ReadLine();
+        Console.WriteLine("ваш возраст: ");
+        string age = Console.ReadLine();
+        Console.WriteLine("ваш город: ");
+        string town = Console.ReadLine();
+        Console.WriteLine("Вас зовут " + name + ", ваш знак зодиака " + sigh + ", вам " + age + " лет, а живёте вы в городе " + town + ".");
+    }
+}
