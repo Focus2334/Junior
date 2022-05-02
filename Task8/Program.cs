f27972332ba6fc5ee5bebc0ebf44192c948c4a75
Focus2334 <51910536+Focus2334@users.noreply.github.com>
2022-05-02 22:15:30 +0500

    Add files via upload

---
+++ Task8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int timeForOnePerson = 10;
+        Console.WriteLine("Введите кол-во старушек:");
+        int numberOfPerson = int.Parse(Console.ReadLine());
+        int timeForWaitInMinutes = timeForOnePerson * numberOfPerson;
+        int timeForWaitInHours = timeForWaitInMinutes / 60;
+        timeForWaitInMinutes = timeForWaitInMinutes - (timeForWaitInHours * 60);
+        if (timeForWaitInHours != 0 & timeForWaitInMinutes != 0)
+        {
+            Console.WriteLine("Вы должны отстоять в очереди {0} часа и {1} минут.", timeForWaitInHours, timeForWaitInMinutes);
+        }
+        else
+        {
+            if (timeForWaitInMinutes != 0)
+            {
+                Console.WriteLine("Вы должны отстоять в очереди {0} минут.", timeForWaitInMinutes);
+            }
+            else
+            {
+                Console.WriteLine("Вы должны отстоять в очереди {0} часа.", timeForWaitInHours);
+            }
+        }
+
+
+    }
+}
