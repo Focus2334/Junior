d3f913086c9f199c2f869b1a4a79e67b0850236c
Focus2334 <51910536+Focus2334@users.noreply.github.com>
2022-05-02 21:41:49 +0500

    Add files via upload

---
+++ Task7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int cristallPrice = 15;
+        Console.WriteLine("Сколько у вас золота?");
+        int totalGold = int.Parse(Console.ReadLine());
+        Console.WriteLine("Сколько кристаллов вы хотите купить за золото по цене {0}?", cristallPrice);
+        int numberOfCristallToBuy = int.Parse(Console.ReadLine());
+        int remainGold = totalGold - (cristallPrice * numberOfCristallToBuy);
+        Console.WriteLine("Вы купили {0} кристаллов", numberOfCristallToBuy);
+        Console.WriteLine("У вас осталось {0} золота", remainGold);
+
+    }
+}
