b4f33362c2c12e69a0c3504c0640f2c973ee6339
Focus2334 <51910536+Focus2334@users.noreply.github.com>
2022-05-02 21:14:48 +0500

    Update Program.cs

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -6,8 +6,8 @@ class Program
     {
         int picturesInRow = 3;
         int allPictures = 52;
-        int rows = allPictures / picturesInRow;
-        int remainder = allPictures % picturesInRow;
-        Console.WriteLine("Всего рядов: " + rows + ", картин сверх меры: " + remainder);
+        int fullyFilledRowsOfPictures = allPictures / picturesInRow;
+        int remainderOfPicturesBeyondMeasure = allPictures % picturesInRow;
+        Console.WriteLine("Всего рядов: " + fullyFilledRowsOfPictures + ", картин сверх меры: " + remainderOfPicturesBeyondMeasure);
     }
 }
