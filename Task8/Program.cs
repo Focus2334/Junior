bc15656484466f96d5157621b41de0bf07a34dc1
Focus2334 <51910536+Focus2334@users.noreply.github.com>
2022-05-02 22:28:27 +0500

    Update Program.cs

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -5,12 +5,12 @@ class Program
     static void Main(string[] args)
     {
         int timeForOnePerson = 10;
-        int MinutesInHour = 60
+        int minutesInHour = 60
         Console.WriteLine("Введите кол-во старушек:");
         int numberOfPerson = int.Parse(Console.ReadLine());
         int timeForWaitInMinutes = timeForOnePerson * numberOfPerson;
-        int timeForWaitInHours = timeForWaitInMinutes / MinutesInHour;
-        timeForWaitInMinutes = timeForWaitInMinutes - (timeForWaitInHours * MinutesInHour);
+        int timeForWaitInHours = timeForWaitInMinutes / minutesInHour;
+        timeForWaitInMinutes = timeForWaitInMinutes - (timeForWaitInHours * minutesInHour);
         if (timeForWaitInHours != 0 & timeForWaitInMinutes != 0)
         {
             Console.WriteLine("Вы должны отстоять в очереди {0} часа и {1} минут.", timeForWaitInHours, timeForWaitInMinutes);
