de351193c5fff884705e264a4ce4ec09fe7c4a20
Focus2334 <51910536+Focus2334@users.noreply.github.com>
2022-05-02 20:46:56 +0500

    Add files via upload

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 3;
+        int allPictures = 52;
+        Console.WriteLine("Всего рядов: " + (allPictures / row) + ", картин сверх меры: " + (allPictures % row));
+    }
+}
